a62de3218e27d16f6e5dd77bd6d40cd366018cb6
Deniz000 <guldenizozdemir03@gmail.com>
2023-03-08 16:36:09 +0300

    completed 5

---
+++ campSpring/rentACar/src/main/java/kodlama/io/rentACar/business/requests/CreateBrandRequest.java
@@ -0,0 +1,16 @@
+package kodlama.io.rentACar.business.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateBrandRequest {
+	//Elimde şöyle bi marka var bunu kaydeder misin?
+	//Api 'yi kullanan son kullanıcıdan istek alıyorum request
+	
+	private String name;
+	
+}
---
+++ campSpring/rentACar/src/main/java/kodlama/io/rentACar/business/responses/GetAllBrandsResponse.java
@@ -0,0 +1,23 @@
+package kodlama.io.rentACar.business.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetAllBrandsResponse {
+//yani tüm brand leri listelemek istediğimizdeki yanıtımız
+	
+	private int id;
+	private String name;
+	
+}
+
+//brand --> id, name, quantity
+//GetAllBransResponse --> id, name 
+
+//tüm id leri alıp buradaki id ye
+//name leri alıp buradaki name e atıyoruz. 
+//Buna da MAPPİNG DENİYOR.
