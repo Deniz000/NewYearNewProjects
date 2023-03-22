31cdf034eba70383c2a0857474041e0e29d75130
Deniz000 <guldenizozdemir03@gmail.com>
2023-03-22 14:50:51 +0300

    -

--- campSpring/rentACar/src/main/java/kodlama/io/rentACar/RentACarApplication.java
+++ campSpring/rentACar/src/main/java/kodlama/io/rentACar/RentACarApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
